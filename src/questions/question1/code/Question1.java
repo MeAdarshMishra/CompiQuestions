dd908ac403a31fd2db3dde2f29f63ee046867be5
dbc2201 <divyanshb22@gmail.com>
2021-01-27 22:56:22 +0530

    add code for Question1n

--- src/questions/question1/code/Question1.java
+++ src/questions/question1/code/Question1.java
@@ -1,4 +1,28 @@
 package questions.question1.code;
 
-public class Question1 {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+class Question1 {
+    // Please try not to change anything in this method.
+    public static void main(String[] args) {
+        int[] numbers = new int[15];
+        addRandomNumbers(numbers);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(findPairs(numbers, 10));
+    }
+
+    // Please try not to change anything in this method.
+    public static void addRandomNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextInt() % 10;
+        }
+    }
+
+    // Try to complete this method.
+    public static List<String> findPairs(int[] numbers, int desiredSum) {
+        return null;
+    }
 }
+
