218569664302e40e7e2dbf0cdd01317f1a6eab03
dbc2201 <divyanshb22@gmail.com>
2021-01-27 22:58:16 +0530

    add code for Question2

---
+++ src/questions/question2/code/Question2.java
@@ -0,0 +1,20 @@
+package questions.question2.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Question2 {
+    public static final int MAX_UNICODE = 65_535;
+
+    public static void main(String[] args) {
+        System.out.println(checkWhetherStringIsUnique("Competitive Programming"));
+    }
+
+    public static boolean checkWhetherStringIsUnique(String string) {
+        Map<Character, Boolean> characters = new HashMap<>();
+
+        // Write your code here...
+
+        return true;
+    }
+}
