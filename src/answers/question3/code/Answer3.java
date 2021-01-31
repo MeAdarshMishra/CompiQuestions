27a918d31a2128bc87202cdf86beddb43cb817e5
dbc2201 <divyanshb22@gmail.com>
2021-01-31 10:44:57 +0530

    compute if a character if already present in the map

--- src/answers/question3/code/Answer3.java
+++ src/answers/question3/code/Answer3.java
@@ -10,6 +10,20 @@ class Question3 {
         for (int index = 0; index < string.length(); index++) {
             // 2. fetch a single character from the string and store it into `currentCharacter` variable
             char currentCharacter = string.charAt(index);
+            // 3. compute if a character if already present in the map
+            // if true, increase the (integer) value by 1
+            // if false, set it to 1
+            resultMap.compute(
+                    currentCharacter, (key, value) -> {
+                        if (value == null) {
+                            return 1;
+                        } else {
+                            return ++value; // pre-fix increment operator is needed, post-fix will not work, try!
+                        }
+                        // or this if-else statement can be summarized as
+                        // (value == null) ? 1 : ++value;
+                    }
+            );
         }
         return resultMap;
     }
