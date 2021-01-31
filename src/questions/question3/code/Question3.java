e3f8980590d4abc6fb6cb6d693e681f5ee647192
dbc2201 <divyanshb22@gmail.com>
2021-01-31 10:31:41 +0530

    add code for question 3

---
+++ src/questions/question3/code/Question3.java
@@ -0,0 +1,17 @@
+package questions.question3.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Question3 {
+    public static Map<Character, Integer> countDuplicateCharacters(String string) {
+        var resultMap = new HashMap<Character, Integer>();
+        // write your code here ...
+        return resultMap;
+    }
+
+    public static void main(String[] args) {
+        String message = "Competitive Programming Can Be A Little Bit Tricky!";
+        System.out.println(countDuplicateCharacters(message));
+    }
+}
