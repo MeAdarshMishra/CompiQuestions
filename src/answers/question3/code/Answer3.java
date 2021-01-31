83f2037a76faebcb818ed9c062f1f52439164964
dbc2201 <divyanshb22@gmail.com>
2021-01-31 10:40:37 +0530

    write a loop to iterate the string

--- src/answers/question3/code/Answer3.java
+++ src/answers/question3/code/Answer3.java
@@ -1,4 +1,21 @@
 package answers.question3.code;
 
-public class Answer3 {
+import java.util.HashMap;
+import java.util.Map;
+
+class Question3 {
+    public static Map<Character, Integer> countDuplicateCharacters(String string) {
+        var resultMap = new HashMap<Character, Integer>();
+        // 1. use a loop to iterate the string, or use toCharArray() with for-each loop
+        for (int index = 0; index < string.length(); index++) {
+
+        }
+        return resultMap;
+    }
+
+    public static void main(String[] args) {
+        String message = "Competitive Programming Can Be A Little Bit Tricky!";
+        System.out.println(countDuplicateCharacters(message));
+    }
 }
+
