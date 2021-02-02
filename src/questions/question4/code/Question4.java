7ec37896ef561c1aadd534212bdddffb99664cb8
dbc2201 <divyanshb22@gmail.com>
2021-02-02 22:46:56 +0530

    write starter code for question 4

---
+++ src/questions/question4/code/Question4.java
@@ -0,0 +1,11 @@
+package questions.question4.code;
+
+public class Question4 {
+
+    public static final int EXTENDED_ASCII_CODES = 256;
+
+    // Complete this method, feel free to overload this method and create an alternate approach for the solution
+    public char findFirstNonRepeatedCharacter(String string) {
+        return '\0';
+    }
+}
